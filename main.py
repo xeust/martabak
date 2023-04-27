a1171f5a4f0e0ce512606ae6f4598d63771b59e9
Max <maxeust@gmail.com>
2023-04-27 12:53:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from jinja2 import Template
 app = FastAPI()
 
 webcrate_key = os.getenv("WEBCRATE_KEY")
+link_key = os.getenv("LINK_KEY")
 
 deta = Deta(webcrate_key)
 
@@ -16,7 +17,7 @@ links = deta.Base("links")
 
 @app.get("/")
 def html_handler():
-    article_html = links.get("8638322465251595-ym309DoXCT")["meta"]["description"]
+    article_html = links.get(link_key)["meta"]["description"]
     home_template = Template((open("index.html").read()))
     home_css = open("style.css").read()
     return HTMLResponse(home_template.render(content=article_html, css=home_css))
